8e7fa60af72bf9d70def93b5226964e1e2eff966
rafael <rafael@IT-Admins-MacBook-Pro.local>
2021-05-21 00:21:26 -0300

    add 404 page

--- pages/clients/[id]/[clientprojectid].js
+++ pages/clients/[id]/[clientprojectid].js
@@ -1,10 +1,8 @@
 import { useRouter } from 'next/router';
-
 function SelectedClientProjectPage() {
   const router = useRouter();
 
   console.log(router.query);
-
   return (
     <div>
       <h1>The Project Page for a Specific Project for a Selected Client</h1>
