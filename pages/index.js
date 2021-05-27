05891bc85c9f265936e0827ae60fe6886e2093c2
rafael <rafael@IT-Admins-MacBook-Pro.local>
2021-05-26 22:57:47 -0300

    add getStaticProps method

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,27 @@
-import Link from 'next/link';
+import path from 'path';
+import fs from 'fs/promises';
 
-function HomePage() {
+function HomePage(props) {
+  const { products } = props;
   return (
-    <div>
-      <h1>The Home Page</h1>
-
-      <ul>
-        <li>
-          <Link href='/portfolio'>Portfolio</Link>
-        </li>
-        <li>
-          <Link href='/clients'>Clients</Link>
-        </li>
-      </ul>
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
 
 export default HomePage;
