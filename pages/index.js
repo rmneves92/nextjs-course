c20d45b06d0119debe74ef69337c51ee87856f52
Rafael Neves <rafa.mneves@hotmail.com>
2021-06-19 10:33:33 -0300

    add api routes

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,86 @@
-import path from 'path';
-import fs from 'fs/promises';
-import Link from 'next/link';
+import { useRef, useState } from "react";
+import path from "path";
+import fs from "fs/promises";
+import Link from "next/link";
+
 function HomePage(props) {
   const { products } = props;
+
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      header: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function loadFeedbackHandler() {
+    fetch("/api/feedback")
+      .then((response) => response.json())
+      .then((data) => setFeedbackItems(data.feedback));
+  }
+
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}>
-          <Link href={`/products/${product.id}`}>{product.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      {/* <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Link href={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+      </ul> */}
+
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef}></input>
+        </div>
+
+        <div>
+          <label htmlFor="email">Your feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+
+        <button>Send feedback</button>
+      </form>
+      <hr />
+
+      <button onClick={loadFeedbackHandler}>Load feedback</button>
+
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
 export async function getStaticProps(context) {
-  console.log('(Re-)Generating...');
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  console.log("(Re-)Generating...");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
   if (!data) {
     return {
       redirect: {
-        destination: '/no-data',
+        destination: "/no-data",
       },
     };
   }
